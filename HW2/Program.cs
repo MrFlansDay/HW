beb626041f5df91c5a4e17c05e66a6d809c4ae16
MrFlansDay <lexa2004924@gmail.com>
2022-06-27 18:46:21 +1000

    HW2

---
+++ HW2/Program.cs
@@ -0,0 +1,18 @@
+﻿int SecondNum(int a)
+{
+   return a / 10 % 10;
+}
+void PrintThirdNum(int a)
+{
+   if (a < 100) Console.WriteLine("No third number");
+   else Console.WriteLine(a.ToString()[2]);
+}
+bool IsDayOff(int a)
+{
+   if (a > 7 || a < 6) return false;
+   else return true;
+}
+
+Console.WriteLine(SecondNum(147));
+PrintThirdNum(14857);
+Console.WriteLine(IsDayOff(6));
